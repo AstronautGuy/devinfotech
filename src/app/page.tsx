1a65a331b28e6ef636b01521acdd45d7e812d152
TheAstronautGuy <theastronautguy2@gmail.com>
2025-09-13 13:30:44 +0530

    added header and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import {HeroSection} from "@/components/HeroSection";
+
 export default function Home() {
   return (
-    <div>
-      <h1>This Website is under construction</h1>
-      <h2>Devolve is building something huge</h2>
-    </div>
+      <main>
+          <HeroSection/>
+      </main>
   );
 }
